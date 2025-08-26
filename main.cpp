41d8eaa5e77f2a405e1903970d5c7845e88cbe4f
Ragunag <ragna@javerianacali.edu.co>
2025-08-26 10:42:15 -0500

    actualizacion de la calculadora simple al volverse cada vez menos simple

--- main.cpp
+++ main.cpp
@@ -1,21 +1,29 @@
 #include <iostream>
-#include "Matematicas.h"  // Incluir el archivo de cabecera con las funciones
+#include "matematicas.h"
+#include "matriz_utils.h"
 
-int main()
-{
+int main() {
     int a, b;
 
-    // Pedir al usuario que ingrese los números
     std::cout << "Ingrese el primer numero: ";
     std::cin >> a;
     std::cout << "Ingrese el segundo numero: ";
     std::cin >> b;
 
-    // Llamar a las funciones y mostrar resultados
     std::cout << "Suma: " << sumar(a, b) << std::endl;
     std::cout << "Resta: " << restar(a, b) << std::endl;
     std::cout << "Multiplicacion: " << multiplicar(a, b) << std::endl;
     std::cout << "Division: " << dividir(a, b) << std::endl;
+
+    int matrizA[2][3] = {{1, 2, 3}, {4, 5, 6}};
+    int matrizB[2][3] = {{10, 20, 30}, {40, 50, 60}};
+    int resultado[2][3];
+
+    sumarMatrices(matrizA, matrizB, resultado);
+
+    imprimirMatriz("Matriz A:", matrizA);
+    imprimirMatriz("Matriz B:", matrizB);
+    imprimirMatriz("Resultado:", resultado);
 
     return 0;
 }
